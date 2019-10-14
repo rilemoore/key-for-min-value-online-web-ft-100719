6a7ecc65ed1afad22ef383a3c703c76d8ef82ed2
Riley Moore <rilemoore@gmail.com>
2019-10-14 23:41:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if(name_hash.size == 0)
+    return nil
+  end
   current_min = 1000000000000000000
   min_key = ""
   name_hash.collect do |name, value|
